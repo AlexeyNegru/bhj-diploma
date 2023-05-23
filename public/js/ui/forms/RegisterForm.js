16c9bc2178a7dd3a7324befdbf9318d0b354a4c8
Alex n <Shakal240@gmail.com>
2023-05-23 15:30:28 +0300

    Done

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -3,13 +3,22 @@
  * регистрации
  * */
 class RegisterForm extends AsyncForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
-   * состояние App.setState( 'user-logged' )
-   * и закрывает окно, в котором находится форма
-   * */
-  onSubmit(data) {
-
-  }
+    /**
+    * Производит регистрацию с помощью User.register
+    * После успешной регистрации устанавливает
+    * состояние App.setState( 'user-logged' )
+    * и закрывает окно, в котором находится форма
+    * */
+    onSubmit(data) {
+        User.register(data, (err, response) => {
+            if (response && response.success === true) {
+                // сбрасываем данные формы
+                this.element.reset();
+                // авторизуем пользователя
+                App.setState('user-logged');
+                // закрываем форму
+                App.getModal('register').close();
+            }
+        });
+    }
 }
